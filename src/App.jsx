8cbf1612d7819b61c9291086d7d09bf09fd38dc7
Diego <dgerman359@gmail.com>
2023-08-01 22:42:16 -0300

    instalación de react y tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App(){
+return (
+    <>
+     <header>Esto va a ser la barra de navegación</header> 
+     <main>Esto va a ser el contenido de la interfaz</main>
+     <footer>Esto va a ser el footer</footer>
+    </>
+  )
+}
+
+export default App
